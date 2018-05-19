f488eb3d7e07a70c64700af18e81df455ebcf710
Jeremiah Metzen <jeremiahmetzen@hotmail.com>
2018-05-19 15:45:59 -0500

    Used the index file to outline a new class hierarchy.

---
+++ BlackjackGame.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jerem
+ * Date: 5/19/2018
+ * Time: 3:17 PM
+ */
+
+// @note I'm constraining the scope of this program to Blackjack-specific stuff only. That means that there may not
+// be a generic "game" interface to implement from. I also don't see a need to create Blackjack game interface
+// if there won't be any implementation-specific variants of the class.
+
+class BlackjackGame
+{
+    public function __construct()
+    {
+
+    }
+}
--- index.php
+++ index.php
@@ -1,196 +1,73 @@
-#!/usr/bin/php
 <?php
 /**
  * Created by PhpStorm.
  * User: jerem
- * Date: 5/12/2018
- * Time: 2:04 PM
+ * Date: 5/19/2018
+ * Time: 3:20 PM
  */
 
-define("PLAYERS_PER_GAME", 5);
+define("NUM_PLAYERS", 5);
 
-require_once("TestHand.php");
-require_once("AggressiveStyle.php");
-require_once("DealerStyle.php");
-require_once("BlackjackDeck.php");
-
-require_once("cards/Two.php");
-require_once("cards/Three.php");
-require_once("cards/Four.php");
-require_once("cards/Five.php");
-require_once("cards/Six.php");
-require_once("cards/Seven.php");
-require_once("cards/Eight.php");
-require_once("cards/Nine.php");
-require_once("cards/Ten.php");
-require_once("cards/Jack.php");
-require_once("cards/Queen.php");
-require_once("cards/King.php");
-require_once("cards/Ace.php");
-
-/*
- * Game
- * Hand
- * Card
- * Player
- *  Dealer
- * PlayStyle
- * ScoreKeeper? (Some kind of DB interface)
- */
-
-// The goal is to play n hands of Blackjack with various play styles, tracking which style statistically does better.
-
-// Not doing much validation. We just need the number of hands to process from the command line.
-// Could take some config options for play styles to use, etc. in the future.
 if(count($argv) == 2){
-    // @note Important to distinguish between a game, and a hand. A hand refers to the cards a player has during a game.
     $intNumGames = $argv[1];
 
-    // @note This means that we're reusing styles throughout the process, and will need to clear hands and other data
-    // between games.
-    // @todo Fix the lack of an empty hand issue.
-    $aPlayersToStyles = array(
-            1 => new AggressiveStyle(),
-            2 => new AggressiveStyle(),
-            3 => new AggressiveStyle(),
-            4 => new AggressiveStyle(),
-            5 => new AggressiveStyle());
+    $objGame = new BlackjackGame();
 
-    $aResults = array();
+    // This will probably need to be an interface.
+    $objResults = new BlackjackResultsLogger();
 
-    $aPlayerHands = array();
+    // @todo Determine where the boundary between games/hands/players is. Do you instantiate a new Game with every hand?
+    // Do you just call something like game->start() or game->deal()?
+    // I think I'm going to stick with my initial method of separating hands from games. So the same game object will
+    // be used throughout, while creating new hands.
+    for($i=1;$i<=NUM_PLAYERS;$i++){
+        // Every x number of players are going to be attributed a different play style.
+        // @note Remember that a play style needs to be able to look at the entire game, ideally.
+        $objPlayStyle = null;
 
-    // $aDeck = array_flip(range(1, 52));
-    $aDeck = array();
-    $objDeck = new BlackjackDeck();
-    $aSuites = array("Clubs", "Diamonds", "Hearts", "Spades");
-    $aCardSequence = array("Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King", "Ace");
-
-    // Fill and shuffle our deck.
-    foreach($aCardSequence as $strCard){
-        foreach($aSuites as $strSuite){
-            $aDeck[] = new $strCard($strSuite);
-            $objDeck->addCard(new $strCard($strSuite));
+        if($i % 2 == 0){
+            $objPlayStyle = new BalancedStyle();
         }
+        elseif($i % 3 == 0){
+            $objPlayStyle = new AggressiveStyle();
+        }
+        elseif($i % 4 == 0){
+            $objPlayStyle = new ConservativeStyle();
+        }
+        else{
+            $objPlayStyle = new RandoStyle();
+        }
+
+        $objPlayer = new BlackjackPlayer($objPlayStyle);
+
+        $objGame->addPlayer($objPlayer);
     }
 
-    shuffle($aDeck);
-    $objDeck->shuffle();
+    $objDealer = new BlackjackDealer(new DealerStyle());
 
-    // echo "Deck:\n" . print_r($aDeck, true) . "\n";
+    // @todo Figure out how to generate a random play style here without resorting to dynamic variables and the like.
+    for($i=1;$i<=$intNumGames;$i++){
+        // @note Dependency is a bit weird here. Normally I'd be inclined to tell the game to deal a new hand.
+        $objHand = new BlackjackHand($objGame);
+        // Alternatively.
+        $objGame->dealHand($objHand);
 
-    echo "Number of hands to process: $intNumGames\n";
-
-    for($intGameNumber=1; $intGameNumber<=1; $intGameNumber++){
-        // $objGame = new Blackjack();
-        // $objGame->setPlayerCount(PLAYERS_PER_GAME);
-
-        // echo "Creating a test play style for game number $intGameNumber\n";
-        $objTestHand = new TestHand();
-        $objTestStyle = new AggressiveStyle($objTestHand);
-
-        // Deal two cards, face up, to each player. One face up and one face down to the dealer.
-        $intCardNum = 0;
-        for($intPlayerNum=1; $intPlayerNum<=PLAYERS_PER_GAME; $intPlayerNum++){
-            // echo "Processing player $intPlayerNum========================\n";
-
-            // @todo Make this an attribute of a Game object.
-            $aResults[$intPlayerNum][$intGameNumber] = array(
-                "total" => "",
-                "state" => "",
-                "result" => "");
-
-            $aNewCards = $objDeck->deal(2);
-            $objHand = new TestHand($aNewCards);
-            $aPlayerHands[$intPlayerNum] = $objHand;
-            $objStyle = $aPlayersToStyles[$intPlayerNum];
-
-            $objStyle->setHand($objHand);
-
-            // For each player in the game, play until a stay, 21, or bust.
-            $intTotal = $objHand->getTotal();
-            $strDecision = $objStyle->decide();
-            $strResult = "";
-            while($strDecision != "stay" && $strResult != "bust" && $strResult != "blackjack"){
-                $objNext = $objDeck->dealOne();
-                // echo "Next:\n" . print_r($objNext, true) . "\n";
-                // echo "Total before adding card: $intTotal\n";
-
-                $objHand->addCard($objNext);
-
-                // @todo Store results in an overall structure.
-                $intTotal = $objHand->getTotal();
-                // echo "Total after adding card: $intTotal\n";
-                if($intTotal > 21){
-                    // echo "[BUST]\n";
-                    $strResult = "bust";
-                }
-                elseif($intTotal == 21){
-                    // echo "[BLACKJACK]\n";
-                    $strResult = "blackjack";
-                }
-                else{
-                    $strDecision = $objStyle->decide();
-                }
-
-                // echo "Player decided: $strDecision\n";
-            }
-
-            $aResults[$intPlayerNum][$intGameNumber]["total"] = $intTotal;
-            $aResults[$intPlayerNum][$intGameNumber]["state"] = $strResult;
-
-            // echo "End processing player $intPlayerNum========================\n";
+        while(!$objGame->isFinished()){
+            $objGame->getPlayerInput();
         }
 
-        // echo "Processing dealer========================\n";
+        $objWinner = $objGame->getWinner(); // Should return a player.
+        $intTotal = $objGame->getPlayerTotal($objWinner);
 
-        // Process play for dealer.
-        $aResults["dealer"][$intGameNumber] = array(
-            "total" => "",
-            "state" => "",
-            "result" => "");
-        // @todo Create some mechanism for representing cards as face up/face down. In standard rules, no one would be able to see the dealer's second card.
-        $aDealerCards = $objDeck->deal(2);
-        $objDealerHand = new TestHand($aDealerCards);
-        $objDealerStyle = new DealerStyle($objDealerHand);
+        // @todo Possibly figure out a way to retrieve all other player data, so you could determine how often a
+        // given style busts, etc.
 
-        $intTotal = $objDealerHand->getTotal();
-        $strDecision = $objDealerStyle->decide();
-        $strResult = "";
-        while($strDecision != "stay" && $strResult != "bust" && $strResult != "blackjack"){
-            $objNext = $objDeck->dealOne();
-                // echo "Next:\n" . print_r($objNext, true) . "\n";
-                // echo "Total before adding card: $intTotal\n";
+        // Going to log this stuff as simple key/value pairs.
+        $objResults->addSet(array(
+            "winner" => $objWinner->getPlayerNumber(),
+            "score" => $intTotal
+        ));
+    }
 
-                $objHand->addCard($objNext);
-
-                // @todo Store results in an overall structure.
-                $intTotal = $objHand->getTotal();
-                // echo "Total after adding card: $intTotal\n";
-                if($intTotal > 21){
-                    // echo "[BUST]\n";
-                    $strResult = "bust";
-                }
-                elseif($intTotal == 21){
-                    // echo "[BLACKJACK]\n";
-                    $strResult = "blackjack";
-                }
-                else{
-                    $strDecision = $objStyle->decide();
-                }
-
-                // echo "Player decided: $strDecision\n";
-            }
-
-            // echo "Last dealer decision: $strDecision, total : $intTotal\n";
-
-            $aResults["dealer"][$intGameNumber]["total"] = $intTotal;
-            $aResults["dealer"][$intGameNumber]["state"] = $strResult;
-
-            // echo "End processing dealer========================\n";
-
-        // Evaluate results.
-
-        echo "Results:\n" . print_r($aResults, true) . "\n";
-    } // Games loop.
+    $objResults->log();
 }
